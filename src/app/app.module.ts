8342964256d6e045b272644b30ea89132caacf33
Rayen023 <56279438+Rayen023@users.noreply.github.com>
2021-07-26 22:10:24 +0100

    Login angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializer } from 'src/utils/app-inits';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { ManagerComponent } from './manager/manager.component';
+import { AdminComponent } from './admin/admin.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessDeniedComponent,
+    ManagerComponent,
+    AdminComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    KeycloakAngularModule, // add keycloakAngular module
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  // add this provider
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [KeycloakService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
