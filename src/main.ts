d596dd1a8288d0a0fcc071f99b0787e61251f88a
Rayen023 <rayen.ghali@insat.u-carthage.tn>
2021-08-19 22:36:10 +0100

    Fixed refresh and access tokens

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { KeycloakService } from 'keycloak-angular';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Keycloak from 'keycloak-js';
+
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +13,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
--- src/utils/app-inits.ts
+++ src/utils/app-inits.ts
@@ -1,6 +1,8 @@
 import { KeycloakService } from 'keycloak-angular';
 import { environment } from '../environments/environment';
 
+
+
 export function initializer(keycloak: KeycloakService): () => Promise<any> {
   return (): Promise<any> => {
     return new Promise(async (resolve, reject) => {
@@ -14,7 +16,7 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
           // If set a false you cannot get any information about the user example the username
           // if you use keycloakService.getUserName() you get this error
           // User not logged in or user profile was not loaded.
-          loadUserProfileAtStartUp: true,
+          // loadUserProfileAtStartUp: true,
           initOptions: {
             //   This is an action we specified on keycloak load
             //   We have two options : 'login-required'|'check-sso'
@@ -29,13 +31,44 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
             //      </body>
             // </html>
             onLoad: 'login-required',
-            checkLoginIframe: true,
+            checkLoginIframe: false,
             // silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
           },
           // By default the keycloak-angular library add Authorization: Bearer TOKEN to all http requests
           // Then to exclude a list of URLs that should not have the authorization header we need to provide  them here.
-          bearerExcludedUrls: ['/assets'],
-        });
+          enableBearerInterceptor: true,
+          bearerExcludedUrls: ['/assets', '/clients/public']
+        })
+        const keycloakAuth = keycloak.getKeycloakInstance();
+
+        keycloakAuth.updateToken(3000);
+        
+        localStorage.setItem("token", keycloakAuth.token || '');
+        localStorage.setItem("refresh-token", keycloakAuth.refreshToken || '');
+
+      
+
+
+        keycloakAuth.onTokenExpired = () => {
+          if (keycloakAuth.refreshToken) {
+            setTimeout(() => {
+              keycloakAuth.updateToken(60).then((refreshed) => {
+                if(refreshed) {
+                  console.log('Token refreshed' + refreshed);
+                  localStorage.setItem("token", keycloakAuth.token || '');
+        localStorage.setItem("refresh-token", keycloakAuth.refreshToken || '');
+                }else{
+                }
+              }).catch(() => {
+                 console.error('Failed to refresh token');
+              });
+           }, 40000)
+            
+          } else {
+            keycloakAuth.login();
+          }
+        }
+      
         resolve(resolve);
       } catch (error) {
         reject(error);
